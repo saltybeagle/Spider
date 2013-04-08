b53c0174b840b22eaa1aa7c5a2f4ad38eebfed5b
Michael Fairchild <mfairchild365@gmail.com>
2013-04-08 15:34:55 -0500

    Add some more pages

---
+++ tests/data/directory1/page1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>directory1/Index</title>
+</head>
+<body>
+<div>
+    These are some links to other pages.
+    <a href='<?php echo $baseurl ?>' title='link to baseurl'>baseurl</a>
+</div>
+</body>
+</html>
+
+    
